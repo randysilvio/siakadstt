cc45c258848d6544b8c41447e709ce5cf26c7712
randy <randy.silvio@gmail.com>
2025-08-10 23:50:55 +0900

    last fix lv 10

--- app/Http/Middleware/DosenMiddleware.php
+++ app/Http/Middleware/DosenMiddleware.php
@@ -1,25 +1,40 @@
 <?php
 
-namespace App\Http\Middleware;
-
-use Closure;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Symfony\Component\HttpFoundation\Response;
 
-class DosenMiddleware
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        if (Auth::check() && Auth::user()->role == 'dosen') {
-            return $next($request);
-        }
+define('LARAVEL_START', microtime(true));
 
-        return redirect('/dashboard')->with('error', 'Halaman ini hanya untuk dosen.');
-    }
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+*/
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
 }
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+*/
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
