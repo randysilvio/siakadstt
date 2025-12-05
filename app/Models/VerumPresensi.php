f82c2452346cf2fc46d427fd620f10984a636a1b
randysilvio <randy.silvio@gmail.com>
2025-12-05 11:01:10 +0900

    presensi

--- app/Models/VerumPresensi.php
+++ app/Models/VerumPresensi.php
@@ -1,18 +1,37 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class VerumPresensi extends Model {
+class VerumPresensi extends Model
+{
     use HasFactory;
-    protected $table = 'verum_presensi';
-    protected $fillable = ['kelas_id', 'judul_pertemuan', 'pertemuan_ke', 'waktu_buka', 'waktu_tutup'];
-    protected $casts = ['waktu_buka' => 'datetime', 'waktu_tutup' => 'datetime'];
 
-    public function kelas() {
+    protected $table = 'verum_presensi';
+
+    protected $fillable = [
+        'kelas_id',
+        'judul_pertemuan',
+        'pertemuan_ke',
+        'waktu_buka',
+        'waktu_tutup'
+    ];
+
+    // PENTING: Casting agar field ini dianggap sebagai Carbon Date Object
+    protected $casts = [
+        'waktu_buka' => 'datetime',
+        'waktu_tutup' => 'datetime',
+    ];
+
+    public function kelas()
+    {
         return $this->belongsTo(VerumKelas::class, 'kelas_id');
     }
-    public function kehadiran() {
+
+    public function kehadiran()
+    {
         return $this->hasMany(VerumKehadiran::class, 'presensi_id');
     }
 }
